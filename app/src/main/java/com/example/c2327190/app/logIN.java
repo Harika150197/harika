2928d23a1041117fef41ff68b4adb17c89518650
Harika150197 <[priya]>
2023-04-21 12:16:30 +0100

    created log in page

--- app/src/main/java/com/example/c2327190/app/logIN.java
+++ app/src/main/java/com/example/c2327190/app/logIN.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LogIn extends AppCompatActivity {
+public class logIN extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
