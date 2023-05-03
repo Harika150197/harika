c241435e5a491c1f547de31e14b2f9a459ab11f9
Harika150197 <[priya]>
2023-05-03 10:18:39 +0100

    Restaurant ineer pages are created successfully

---
+++ app/src/main/java/com/example/c2327190/app/Rest3.java
@@ -0,0 +1,14 @@
+package com.example.c2327190.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rest3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rest3);
+    }
+}
